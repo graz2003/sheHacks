a642880cf89bd01b1eb33838f8fbf2533e63da0e
graz2003 <zhougrace2003@gmail.com>
2023-01-14 15:44:25 -0500

    update

--- game.js
+++ game.js
@@ -2,23 +2,49 @@
 var player1Score = 0;
 var player2Score = 0;
 
-var currPlayer = 1;
-
 var p1PlanContinueBtn = document.getElementById('p1-plan-button');
+var p1inputBarginningChips = "";
+var p1inputKeyInterests = "";
 
 var isSubmitted = false;
 var isPlayer1Walkaway = false;
 var isPlayer2Walkaway = false;
 
-p1PlanContinueBtn.addEventListener("click", () => {
-    document.getElementById('planning-round-intro');
-});
+function game() {
+    planRoundIntro(1);
+    planRound(1);
+    planRoundIntro(2);
+    planRound(2);
+
+    negotiationRound();
+}
+
+function planRoundIntro(currPlayer) {
+    if (currPlayer == 1) {
+        document.getElementById('plan-intro-title').innerHTML = "Planning Round: Player 1";
+    } else if (currPlayer == 2) {
+        document.getElementById('plan-intro-title').innerHTML = "Planning Round: Player 2"; 
+    }
+}
 
 function planRound(currPlayer) {
-    if (currPlayer == 1) {
-        h1.innerHTML = "Player 1";
+    if(currPlayer == 1) {
+        document.getElementById('needs').innerHTML = ;
+        document.getElementById('key-interests').innerHTML = ;
+        p1inputBarginningChips = document.getElementById('barginning-chips');
+        p1inputKeyInterests = document.getElementById('walkaway-options');
+
+    } else if(currPlayer == 2) {
+        document.getElementById('needs').innerHTML = ;
+        document.getElementById('key-interests').innerHTML = ;
+        p2inputBarginningChips = document.getElementById('barginning-chips').value;
+        p2inputKeyInterests = document.getElementById('walkaway-options').value;
 
     }
+}
+
+function negotiationRound() {
+    
 }
 
 submiBtn.addEventListner("click", () => { 
