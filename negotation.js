6d2853e6e2e9cf0ce780fe65f043da12e2c13765
vekayy <97319522+vekayy@users.noreply.github.com>
2023-01-14 23:09:54 -0500

    Add files via upload
    new

--- negotation.js
+++ negotation.js
@@ -1,35 +1,15 @@
 // Click button start timer
-    function timerCount () {
-        const timer = document.getElementById('timer');
+var counter = 60;
+  setInterval( function(){
+    counter--;
+    if (counter>=0){
+      id = document.getElementById("timer");
+      id.innerHTML = counter;
+    }
+  }, 1000);
 
-        // Create variables
-        let timerCounter;
-        let seconds = 60;
-        
-        // set counter(interval)
-        timerCounter = setInterval(function () {
-        seconds--;
-        
-        // once time is up, reset timer
-        if (seconds==0){
-            clearInterval(timerCounter);
-            alert('Time up next player. Click continue to start timer');
-        }
+  document.querySelector('#submitButton1').addEventListener('click', function(){ alert ("The button was clicked!") });
+  
+  document.querySelector('#walkButton1').addEventListener('click', function(){ alert ("The button1 was clicked!") });
 
-        // Check if submit button clicked for both users
-        if((document.getElementById('submitButton1').clicked == true) && (document.getElementById('submitButton2').clicked == true)){
-            alert("Success");
-        }
-
-        // Check if anyone walked away
-        //Player 1 check
-        if(document.getElementById('walkButton1').clicked == true) {
-            alert("Player 1 walked out");
-        }
-        //Player 2 check
-        if(document.getElementById('walkButton2').clicked == true) {
-            alert("Player 2 walked out");
-        }
-
-        }, 1000);
-}
+  document.querySelector('#walkButton2').addEventListener('click', function(){ alert ("The button2 was clicked!") });
