dc1ca7e9bece64d3c46c66c4a0d9110e8e6d3a18
graz2003 <zhougrace2003@gmail.com>
2023-01-14 22:18:03 -0500

    hi

--- game.js
+++ game.js
@@ -1,7 +1,4 @@
 
-export var player1Score = 0;
-export var player2Score = 0;
-
 //planning round start
 var player1btn = document.getElementById('p1Btn');
 var player2btn = document.getElementById('p2Btn');
@@ -9,9 +6,10 @@ var player2btn = document.getElementById('p2Btn');
 
 player1btn.addEventListener("click", () => { 
     player1btn.setAttribute("style", "opacity:0.5");
-    
+    console.log("hi");
 });
 
 player2btn.addEventListener("click", () => { 
     player2btn.setAttribute("style", "opacity:0.5");
+    console.log("hi");
 });
