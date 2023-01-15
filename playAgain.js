cee78d3d7cd22a858b1189324f82738f1c03ddb9
vekayy <97319522+vekayy@users.noreply.github.com>
2023-01-14 23:20:17 -0500

    Add files via upload
    new

--- playAgain.js
+++ playAgain.js
@@ -1,17 +1,11 @@
-
-document.getElementById('p1reset-score').addEventListener("click", function (){
+reset.addEventListener("click", function (){
     resetGame();
-    console.log("reset");
-});
 
-document.getElementById('p2reset-score').addEventListener("click", function (){
-    resetGame();
-    console.log("reset");
 });
 
 function resetGame(){
-    player1Score = 0; 
-    Player2Score = 0; 
+    var player1Score = 0; 
+    var Player2Score = 0; 
 }
 
 
